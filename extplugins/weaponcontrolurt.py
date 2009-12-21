0afdbe3d7d31a23995ac689f8c9cd0b8d739a5e7
Courgette <courgette@ubu-team.org>
2009-12-21 21:38:10 +0100

    compatible with iourt41 v1.7+ while keeping backward compatibility

--- extplugins/weaponcontrolurt.py
+++ extplugins/weaponcontrolurt.py
@@ -9,7 +9,7 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -32,16 +32,24 @@
 # - Fix bug with the AK.
 # - Add missing 'kevlar helmet'
 # - Broadcast message when a weapon/item is allowed/disallowed
+# 21/12/2009 - 0.4 - Courgette
+# - can work with Iourt41 1.7+ while keaping it compatible with pre-1.7
+# - fix minor bug where forbidden weapon codes could be found multiple
+#   times in the _forbiddenWeapons list.
+# - add tests
+#
+
 """
 Urban Terror doc on gear : http://www.urbanterror.net/urt_manual/gear.htm
 """
 
-__version__ = '0.3.0'
+__version__ = '0.4'
 __author__  = 'Courgette'
 
 import b3, re
 import b3.events
 import b3.plugin
+import distutils.version
 
 
 #--------------------------------------------------------------------------------------------------
@@ -76,12 +84,27 @@ class WeaponcontrolurtPlugin(b3.plugin.Plugin):
   }
   
   def onStartup(self):
-    # note: we are listening to client name change events as every time the parser receive a clientInfo line, 
-    # the name is set again, hence raising this event.
-    self.registerEvent(b3.events.EVT_CLIENT_NAME_CHANGE)
+    
+    import b3.parsers.iourt41
+    if not isinstance(self.console, b3.parsers.iourt41.Iourt41Parser) \
+        and not isinstance(self.console, b3.fake.FakeConsole):
+        self.error('this plugin requires the iourt41 parser')
+        self.disable()
+    
+    iourt41_version = distutils.version.LooseVersion(b3.parsers.iourt41.__version__) 
+    self.debug('iourt41 version: %s' % iourt41_version)
+    
+    if iourt41_version < distutils.version.LooseVersion('1.7'):
+        # note: we are listening to client name change events as every time the parser receive a clientInfo line, 
+        # the name is set again, hence raising this event.
+        self.registerEvent(b3.events.EVT_CLIENT_NAME_CHANGE)
+    else:
+        self.registerEvent(b3.events.EVT_CLIENT_GEAR_CHANGE)
+        self.registerEvent(b3.events.EVT_CLIENT_TEAM_CHANGE)
+    
+          
           
   def onLoadConfig(self):
-
     # get the admin plugin 
     self._adminPlugin = self.console.getPlugin('admin')
     if not self._adminPlugin:
@@ -105,7 +128,8 @@ class WeaponcontrolurtPlugin(b3.plugin.Plugin):
     for weapon in self.config.options('weapons'):
       try:
         if not self.config.getboolean('weapons', weapon):
-          self._forbiddenWeapons.append(self.weaponCodes[weapon])
+          if self.weaponCodes[weapon] not in self._forbiddenWeapons:
+              self._forbiddenWeapons.append(self.weaponCodes[weapon])
       except:
         self.warning('config error for weapon %s'%weapon)
     
@@ -114,8 +138,11 @@ class WeaponcontrolurtPlugin(b3.plugin.Plugin):
     
         
   def onEvent(self, event):  
-    if event.type == b3.events.EVT_CLIENT_NAME_CHANGE:
+    if event.type == b3.events.EVT_CLIENT_TEAM_CHANGE \
+        or event.type == b3.events.EVT_CLIENT_NAME_CHANGE \
+        or event.type == b3.events.EVT_CLIENT_GEAR_CHANGE:
       self.checkClient(event.client)
+       
        
   def getCmd(self, cmd):
     cmd = 'cmd_%s' % cmd
@@ -131,7 +158,7 @@ class WeaponcontrolurtPlugin(b3.plugin.Plugin):
     if client.team == b3.TEAM_SPEC:
       return
       
-    self.debug('gear : %s'%client.gear)
+    self.debug('%s\'s gear : %s' % (client.name, client.gear))
     
     problems = []
     for weap in self._forbiddenWeapons:
@@ -261,3 +288,124 @@ class WeaponcontrolurtPlugin(b3.plugin.Plugin):
 def find_key(dic, val):
     """return the key of dictionary dic given the value"""
     return [k for k, v in dic.items() if v == val][0]
+
+
+
+
+if __name__ == '__main__':
+    from b3.fake import fakeConsole
+    from b3.fake import FakeClient
+    from b3.fake import joe
+    from b3.fake import superadmin
+    import time
+    import b3.parsers.iourt41
+    from b3.config import XmlConfigParser
+    
+    class FakeUrtClient:
+        __newStyle = False
+        def changesGear(self, newGearString):
+            print "\n%s changes gear to \"%s\"" % (self.name, newGearString) 
+            self.gear = newGearString
+            if FakeUrtClient.__newStyle:
+                self.pushEvent(b3.events.Event(b3.events.EVT_CLIENT_GEAR_CHANGE, newGearString, self)) 
+            else:
+                self.pushEvent(b3.events.Event(b3.events.EVT_CLIENT_NAME_CHANGE, self.name, self)) 
+         
+    ## use mixins to add methods to FakeClient
+    FakeClient.__bases__ += (FakeUrtClient,)
+
+    fakeConsole.Events.createEvent('EVT_CLIENT_GEAR_CHANGE', 'Client gear change')
+    
+    conf = XmlConfigParser()
+    conf.setXml("""
+    <configuration plugin="weaponcontrolurt">
+      
+      <settings name="commands">
+        <!-- min level required to use the weaponcontrol command         
+            0 : guest
+            1 : user
+            2 : regular
+            20 : moderator
+            40 : admin
+            60 : fulladmin
+            80 : senioradmin
+            100 : superadmin
+          -->
+            <set name="weaponcontrol-wpctrl">60</set>
+        </settings>
+      
+        <!-- put '0' below to disallow a weapon, put '1' to allow -->
+        <settings name="weapons">
+            <set name="Beretta 92G">0</set>
+            <set name="Desert Eagle">1</set>
+            <set name="SPAS-12">1</set>
+            <set name="MP5K">1</set>
+            <set name="UMP45">1</set>
+            <set name="HK69">1</set>
+            <set name="LR300ML">1</set>
+            <set name="M4A1">1</set>
+            <set name="G36">1</set>
+            <set name="PSG-1">1</set>
+            <set name="SR-8">1</set>
+            <set name="AK-103">1</set>
+            <set name="Negev">1</set>
+            <set name="HE Grenade">1</set>
+            <set name="Smoke Grenade">0</set>
+            <set name="Kevlar Vest">1</set>
+            <set name="Kevlar Helmet">1</set>
+            <set name="Silencer">1</set>
+            <set name="Laser Sight">1</set>
+            <set name="Medkit">1</set>
+            <set name="NVGs">1</set>
+            <set name="Extra Ammo">1</set>
+        </settings>
+    </configuration>
+    """)
+    
+    
+    def testPlugin(p):
+        joe.connects(1)
+        
+        joe.gear = None
+        joe.team = b3.TEAM_SPEC
+        joe.changesGear('GLAOWRA')
+        
+        joe.gear = None
+        joe.team = b3.TEAM_BLUE
+        joe.changesGear('GLAOWRA')
+    
+        print "\nJoe takes smoke grenades"
+        joe.changesGear('GLAQWRA')
+        time.sleep(1)
+            
+        print "\nJoe goes to red team"
+        joe.team = b3.TEAM_RED
+        time.sleep(1)
+        
+        print "\nJoe takes HE grenades and goes to RED"
+        joe.changesGear('GLAOWRA')
+        joe.team = b3.TEAM_RED
+        
+        superadmin.connects(0)
+        superadmin.says('!wpctrl -he')
+        
+        time.sleep(2)
+        joe.disconnects()
+        superadmin.disconnects()
+    
+    
+    print "\n\n============[ test with iourt41 1.1 ]==============\n"
+    b3.parsers.iourt41.__version__ = '1.1'
+    p = WeaponcontrolurtPlugin(fakeConsole, conf)
+    p.onStartup()
+    testPlugin(p)
+    
+    print "\n\n============[ test with iourt41 1.7+ ]==============\n"
+    b3.parsers.iourt41.__version__ = '1.7'
+    FakeUrtClient.__newStyle = True
+    del p
+    p = WeaponcontrolurtPlugin(fakeConsole, conf)
+    p.onStartup()
+    testPlugin(p)
+    
+  
